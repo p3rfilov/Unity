c0b2d53e610c8a84aabbf219a843904473fb3f21
Andrej Perfilov [home] <33530333+p3rfilov@users.noreply.github.com>
2019-02-13 22:11:45 +0000

    initial commit

---
+++ Test_Project/Assets/Scripts/FollwPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FollwPlayer : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+
+    void Update()
+    {
+        transform.position = player.position + offset;
+
+        if (Input.GetKey(KeyCode.Escape)) // shouldn't be here. Restarts the game
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
---
+++ Test_Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
