48a6d69c76208f833645baef5a49941f942c5ed2
Andrej Perfilov [home] <33530333+p3rfilov@users.noreply.github.com>
2019-11-23 23:36:32 +0000

    completed Spawn Zones

--- Catlike Coding/Assets/Scenes/06 - Object Management/SpawnZone.cs
+++ Catlike Coding/Assets/Scenes/06 - Object Management/SpawnZone.cs
@@ -2,23 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnZone : MonoBehaviour
+public abstract class SpawnZone : MonoBehaviour
 {
-    [SerializeField]
-    bool surfaceOnly;
-
-    public Vector3 SpawnPoint
-    {
-        get
-        {
-            return transform.TransformPoint(surfaceOnly ? Random.onUnitSphere : Random.insideUnitSphere);
-        }
-    }
-
-    private void OnDrawGizmos ()
-    {
-        Gizmos.color = Color.cyan;
-        Gizmos.matrix = transform.localToWorldMatrix;
-        Gizmos.DrawWireSphere(Vector3.zero, 1f);
-    }
+    public abstract Vector3 SpawnPoint { get; }
 }
